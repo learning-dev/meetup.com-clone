3a812fd71e6a1fb003b2736d8db6402281c3ee04
learning-dev <ayazm.140895@gmail.com>
2019-10-25 21:54:59 +0530

    add login page

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms  import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-	username=StringField('Username',
-							validators=[DataRequired(), Length(min=3, max=20)])
+	username = StringField('Username',
+							validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email',
 							validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
