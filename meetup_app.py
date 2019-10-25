19646f594ced146cc811a5ef5f86a909e7d94c94
learning-dev <ayazm.140895@gmail.com>
2019-10-25 19:47:34 +0530

    add register form

--- meetup_app.py
+++ meetup_app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'bfb7bd64347d385dc9fab14e45fbeeeb6d2cef32'
 
 posts = [
 	{
@@ -23,7 +24,7 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
 	return render_template('home.html', posts=posts)
 
 
@@ -31,6 +32,18 @@ def hello():
 def about():
 	return render_template('about.html')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return (redirect(url_for('home')))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
