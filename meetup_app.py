d5db5ffadc9026d5cf15d438b69cf0ac25bf9ac0
learning-dev <ayazm.140895@gmail.com>
2019-10-26 11:51:14 +0530

    add navigation links on the home page

--- meetup_app.py
+++ meetup_app.py
@@ -40,9 +40,15 @@ def register():
 		return (redirect(url_for('home')))
 	return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash(f'Logged In Successfully', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccesful. Please check username	and password', 'danger')
 	return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
