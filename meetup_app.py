9778f5703e67829212266bb0491cf89fff81d845
learning-dev <ayazm.140895@gmail.com>
2019-10-26 14:49:34 +0530

    add database to web application

--- meetup_app.py
+++ meetup_app.py
@@ -1,9 +1,38 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bfb7bd64347d385dc9fab14e45fbeeeb6d2cef32'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+print(app)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	meetups = db.relationship('Meetup', backref='author', lazy=True)
+
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Meetup(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	meetup_name = db.Column(db.String(100	),  nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	organizer = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
+	def __repr__(self):
+		return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
 	{
