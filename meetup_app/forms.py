6eb61363e984e0a6385c60fd21d56f19b14e46c7
learning-dev <ayazm.140895@gmail.com>
2019-11-04 21:53:44 +0530

    add profile picture feature

--- meetup_app/forms.py
+++ meetup_app/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms  import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from meetup_app.models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -34,4 +37,25 @@ class LoginForm(FlaskForm):
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username',
+							validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email',
+							validators=[DataRequired(), Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+	submit = SubmitField('Update')
+
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+			 	raise ValidationError('that username is taken. Please choose another one')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				 raise ValidationError('that email is taken. Please choose another one')
 
--- meetup_app/routes.py
+++ meetup_app/routes.py
@@ -1,6 +1,9 @@
+import secrets
+import os
+from PIL import Image
 from flask import Flask, render_template, url_for, flash, redirect, request
 from meetup_app import app, bcrypt
-from meetup_app.forms import RegistrationForm, LoginForm
+from meetup_app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from meetup_app.models import User, Meetup, db
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -67,7 +70,38 @@ def logout():
 	return redirect(url_for('home'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/profile_pics/' + picture_fn )
+
+	output_size = (125, 125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+
+	i.save(picture_path)
+
+
+	return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-	return render_template('account.html', title='Account')
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		if form.picture.data:
+			picture_file = save_picture(form.picture.data)
+			current_user.image_file = picture_file
+
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your account has been updated!', 'success')
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+	img_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+	return render_template('account.html', title='Account',
+							image_file=img_file, form=form)
